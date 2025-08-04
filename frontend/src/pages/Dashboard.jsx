200c2f988b91538a401a1ec67aae36539a159039
quantumcoderr <47424629+quantum-coderr@users.noreply.github.com>
2025-08-04 19:44:14 +0000

    little errors

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,32 +5,32 @@ import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
 
 export const Dashboard = () => {
-    const [balance, setBalance] = useState(null);
+    // const [balance, setBalance] = useState(null);
 
-    useEffect(() => {
-        const fetchBalance = async () => {
-            try {
-                const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
-                    headers: {
-                        Authorization: "Bearer " + localStorage.getItem("token")
-                    }
-                });
+    // useEffect(() => {
+    //     const fetchBalance = async () => {
+    //         try {
+    //             const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+    //                 headers: {
+    //                     Authorization: "Bearer " + localStorage.getItem("token")
+    //                 }
+    //             });
 
-                setBalance(response.data.balance);
-            } catch (err) {
-                console.error("Error fetching balance", err);
-                setBalance("Error");
-            }
-        };
+    //             setBalance(response.data.balance);
+    //         } catch (err) {
+    //             console.error("Error fetching balance", err);
+    //             setBalance("Error");
+    //         }
+    //     };
 
-        fetchBalance();
-    }, []);
+    //     fetchBalance();
+    // }, [balance]);
 
     return (
         <div>
             <Appbar />
             <div className="m-8">
-                <Balance balance={balance !== null ? `₹${balance.toFixed(2)}` : "Loading..."} />
+                <Balance balance={"10,000"} />
                 <Users />
             </div>
         </div>
